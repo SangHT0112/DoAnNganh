089afb454b9550698bf255925db670ac3eb0de0c
fish <macarada77@gmail.com>
2024-10-19 08:07:10 +0700

    14_Create cardlist

--- DoAnFinal/Web_DoTheThao/Web_DoTheThao/Controllers/CartController.cs
+++ DoAnFinal/Web_DoTheThao/Web_DoTheThao/Controllers/CartController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web_DoTheThao.Models;
+using Web_DoTheThao.Models.ViewModel;
+using Web_DoTheThao.Repository;
 
 namespace Web_DoTheThao.Controllers
 {
 	public class CartController : Controller
 	{
+		private readonly DataContext _dataContext;
+		public CartController(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
 		public IActionResult Index()
 		{
-			return View();
+			List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+			CartItemViewModel cartVM = new()
+			{
+				CartItems = cartItems,
+				GrandToTal = cartItems.Sum(x => x.Quantity * x.Price)
+
+			};
+			return View(cartVM);
 		}
 	}
 }
