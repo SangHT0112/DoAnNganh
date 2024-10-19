9cfaf112b8897190449f526f3e46096dd971b9ef
fish <macarada77@gmail.com>
2024-10-19 19:40:13 +0700

    18_Create Page Admin

---
+++ DoAnFinal/Web_DoTheThao/Web_DoTheThao/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_DoTheThao.Repository;
+
+namespace Web_DoTheThao.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ProductController : Controller
+	{
+		
+		private readonly DataContext _dataContext;
+		public ProductController(DataContext context)
+		{
+			_dataContext = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
